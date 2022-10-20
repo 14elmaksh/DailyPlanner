bacaaf00713a0c48841feac79ef0613df35afad7
14elmaksh <106193970+14elmaksh@users.noreply.github.com>
2022-10-20 14:16:56 -0400

    fixed input box

--- Assets/javascript/script.js/script.js
+++ Assets/javascript/script.js/script.js
@@ -1,3 +1,10 @@
 var currentDay = document.querySelector("#currentDay");
 
 currentDay.textContent = moment().format("dddd, MMMM Do, YYYY");
+
+//Get input textarea elements:
+ // var elementName = document.getElementById("");
+ // var elementName = document.getElementByClassName("");
+// var elementName = document.querySelector("");
+// var elementName = document.querySelectorAll("");
+ 
