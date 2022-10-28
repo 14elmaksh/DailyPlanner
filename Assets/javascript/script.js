ee04f9f85dfaccd3b44869f9349673c496cd695c
14elmaksh <106193970+14elmaksh@users.noreply.github.com>
2022-10-28 15:49:30 -0400

    input var

---
+++ Assets/javascript/script.js
@@ -0,0 +1,45 @@
+
+
+
+
+var nine = document.getElementById("9am").value;
+var ten = document.getElementById("10am").value;    
+var eleven = document.getElementById("11am").value;
+var twelve = document.getElementById("12pm").value;
+var one = document.getElementById("1pm").value;
+var two = document.getElementById("2pm").value;
+var three = document.getElementById("3pm").value;
+var four = document.getElementById("4pm").value;
+
+// console.log(nine.value);
+
+
+localStorage.setItem("time", nine;)
+
+// var saveB = document.querySelector(".saveBtn");
+
+
+
+
+// var inputDis = document.querySelector(".notesD");
+
+
+// // get input values from form
+// inputBox.value = localStorage.getItem("notesT");
+
+// // onclick function to store retrieved form input
+// $("saveBtn").on("click", function(){
+//     localStorage.setItem("notesT", inputBox.value)
+//     console.log($(this));
+// })
+
+
+
+//Get input textarea elements:
+ // var elementName = document.getElementById("");
+ // var elementName = document.getElementByClassName("");
+// var elementName = document.querySelector("");
+// var elementName = document.querySelectorAll("");
+ 
+// var currentDay = document.querySelector("#currentDay");
+// currentDay.textContent = moment().format("dddd, MMMM Do, YYYY");
